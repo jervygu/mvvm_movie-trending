d8d0d6f70ea622e8cd417996e9e906865effb2ac
Jervy Umandap <jegumandap@unionbankph.com>
2024-06-06 08:50:29 +0800

    - Starter project

---
+++ MovieTrending/MovieTrending/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieTrending
+//
+//  Created by Jervy Umandap on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
